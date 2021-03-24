4409a09e03c32d1c09a04c970507d09f7bd48932
Nick Campion <nickcampion88@gmail.com>
2021-03-24 10:46:14 +0000

    initial commit

--- get_pi.rb
+++ get_pi.rb
@@ -4,7 +4,11 @@ require 'bunny'
 require "bigdecimal/math"
 include BigMath
 
-rabbit_host = ENV["rabbit_host"] || 127.0.0.1
+puts "[x] Starting up..."
+
+rabbit_host = ENV["rabbit_host"] || "127.0.0.1"
+
+puts "[x] Using #{rabbit_host} for RabbitMQ..."
 
 connection = Bunny.new(automatically_recover: false, host: rabbit_host, log_level: 'warn')
 connection.start
@@ -16,7 +20,6 @@ len = channel.queue_declare('foo', durable: true)
 
 queue = channel.queue('foo', durable: true, auto_delete: false)
 
-puts "Starting up..."
 
 while len.message_count = 0
   puts "[x] Waiting for Queue to be be populated"
@@ -30,7 +33,7 @@ puts "[x] Queue has #{len.message_count} in the queue, starting"
 while len.message_count > 0
   delivery_info, properties, payload = queue.pop
   puts "[x] Working out Pi to #{payload *1000 } points of accuracy..."
-  puts "Pi: #{BigMath.PI(payload.to_i).to_s('F')}"
+  puts "[x] Pi: #{BigMath.PI(payload.to_i).to_s('F')}"
   len = channel.queue_declare('foo', durable: true)
 end
 
